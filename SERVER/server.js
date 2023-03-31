d70ab1345242cd0c8e072bab15257faf4020fd64
AghajanyanA <astara@null.net>
2023-04-01 02:44:36 +0400

    added online users, message send & receive

--- SERVER/server.js
+++ SERVER/server.js
@@ -12,12 +12,33 @@ const io = new Server(server, {
   }
 });
 
+const _online_users = []
+
 io.on('connection', socket => {
   console.log('A user has connected: ', socket.id)
 
+  io.emit('online-users', _online_users)
+
+  socket.on('active-channel', channelName => {
+    socket.join(channelName)
+  }) 
+
   socket.on('disconnect', () => {
-    console.log('A user has disconnected', socket.id);
+    _online_users.splice(_online_users.findIndex(item => item.socket === socket.id), 1)
+    io.emit('online-users', _online_users)
   });
+
+  socket.on('send-message', message_data => {
+    const senderName = _online_users.find(user => user.socket === message_data.sender)
+
+    const newMsgData = {
+      message: message_data.message,
+      sender: senderName?.username,
+      channel: message_data.channel
+    }
+    console.log(message_data)
+    io.to(message_data.channel).emit('receive-message', newMsgData)
+  })
 
   socket.on('handle-login', data => { // USER LOGIN
     fs.readFile('userData.json', 'utf8', (error, fileData) => {
@@ -25,9 +46,11 @@ io.on('connection', socket => {
         console.error(error)
       } else {
         const currentData = JSON.parse(fileData)
-        if (currentData.find(item => item.username === data.username && item.password === data.password)) {
+        if (currentData.find(item => item.username === data.username && item.password === data.password)) { // LOGIN SUCCESS
           socket.emit('successful-login', socket.id)
-        } else {
+          _online_users.push({username: data.username, socket: socket.id})
+          io.emit('online-users', _online_users)
+        } else {                  // LOGIN NOT SUCCESS
           socket.emit('unsuccessful-login')
         }
       }
@@ -36,7 +59,7 @@ io.on('connection', socket => {
 
   socket.on('create-users-file--handle-register', data => { // USER REGISTER
     if (data.username.length < 256 && data.username.length < 256) {
-      if(fs.existsSync('userData.json')) { // check if file exists, then add data to it
+      if(fs.existsSync('userData.json')) { // IF FILE EXISTS ADD DATA
         fs.readFile('userData.json', 'utf8', (err, currentData) => {
           if (err) {
             console.error(err);
@@ -49,16 +72,18 @@ io.on('connection', socket => {
             if (err) {
               socket.emit('user-register-fail', `Server error: ${JSON.stringify(err)}`);
             }
-            socket.emit('user-register-success', socket.id) // add listener on front
+            socket.emit('user-register-success', socket.id) 
+            _online_users.push({username: data.username, socket: socket.id})
+            io.emit('online-users', _online_users)
           });
         });
   
-      } else { // if file doesn't exist, create new one, and add data to it
+      } else { // IF FILE DOES NOT EXIST, CREATE FILE AND ADD DATA
         fs.writeFile('userData.json', JSON.stringify([{...data}]), err => {
           if (err) {
-            socket.emit('user-register-fail', `Server error: ${JSON.stringify(err)}`); //add listener on front
+            socket.emit('user-register-fail', `Server error: ${JSON.stringify(err)}`); 
           } else {
-            socket.emit('user-register-success'); //add listener on front
+            socket.emit('user-register-success', socket.id);
           }
         });
       }
@@ -68,7 +93,7 @@ io.on('connection', socket => {
   });
 });
 
-// Start the server
+
 const port = process.env.PORT || 3001;
 server.listen(port, () => {
   console.log(`Server started on port ${port}`);
